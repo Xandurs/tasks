740ed8035340925118f52cec837a34f28e3217d7
Xandurs <xanderlim@live.com.sg>
2026-09-18 17:12:18 +0800

    add code to count deadline using streams

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -32,6 +32,14 @@ public class Main {
         }
         return count;
     }
+    private static int countDeadlineUsingStreams(ArrayList<Task> tasks) {
+        int count = (int) tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .count();
+        return count;
+    }
+
+
 
     public static void printAllData(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
